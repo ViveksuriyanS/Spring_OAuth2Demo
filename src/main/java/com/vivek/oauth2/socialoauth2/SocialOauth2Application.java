ddd139a2b29f07cf71c236c7ccd52daf45b35e47
ViveksuriyanS <viveksuriyan.subramani@broadcom.com>
2025-02-16 23:14:46 +0530

    Changes related to OAuth2 App

---
+++ src/main/java/com/vivek/oauth2/socialoauth2/SocialOauth2Application.java
@@ -0,0 +1,13 @@
+package com.vivek.oauth2.socialoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialOauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/vivek/oauth2/socialoauth2/SocialOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.oauth2.socialoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
